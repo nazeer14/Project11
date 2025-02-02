79ce13aeaea9a59b69bd706b759569a436bb8aa7
nazeer14 <133495211+nazeer14@users.noreply.github.com>
2025-02-02 21:26:14 +0530

    Update UpdateProfileDAO.java

--- Java/UpdateProfileDAO.java
+++ Java/UpdateProfileDAO.java
@@ -8,17 +8,15 @@ public class UpdateProfileDAO
 	
 	public int update(UserBean ub)
 	{
-		String query="update TableName set FIRSTNAME=? , LASTNAME=?, MAIL_ID=?, PHONE=? where username=?";
+		System.out.println(ub);
 		int rc=0;
 		try {
 			Connection con=DBconnect.getcon();
-			PreparedStatement ps=con.prepareStatement(query);
-			
+			PreparedStatement ps=con.prepareStatement("update TableName set FIRSTNAME=? , LASTNAME=?, MAIL_ID=?, PHONE=? where USERNAME=?");
 			ps.setString(1, ub.getFirstname());
 			ps.setString(2, ub.getLastname());
 			ps.setString(3, ub.getMail_id());
 			ps.setString(4, ub.getPhone());
-			
 			ps.setString(5, ub.getUsername());
 			
 			rc=ps.executeUpdate();
